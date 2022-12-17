9e6c1c81a2edecacac4d1fa2b6eb274bf402151b
Allen Frank Cutinha <allencutinha@gmail.com>
2022-12-17 15:01:19 +0100

    formatted the code

--- main.cpp
+++ main.cpp
@@ -1,19 +1,23 @@
-#include<iostream>
-#include<opencv2/opencv.hpp>
+#include <iostream>
+#include <opencv2/opencv.hpp>
 using namespace std;
 
-int main (int argc, char** argv){
-    if(argc <2 ){
-        std::cerr<<"Enter a valid input image path\n";
+void checkImageValidity(cv::Mat const &img, std::string const &file) {
+    if (img.data == nullptr) {
+        std::cerr << "File : " << file << " could not be read !!!" << std::endl;
+        std::cerr << "Please check the file path and try again !!!\n";
+        exit(2);
+    }
+}
+
+int main(int argc, char **argv) {
+    if (argc < 2) {
+        std::cerr << "Enter a valid input image path\n";
         return 1;
     }
-    cv::Mat img;
-    img = cv::imread(argv[1], 1);
-    if(img.data == nullptr){
-        std::cerr<<"File : "<<argv[1]<<" could not be read !!!"<<std::endl;
-        std::cerr<<"Please check the file path and try again !!!\n";
-        return 2;
-    }
+    cv::Mat img = cv::imread(argv[1], 1);
+    checkImageValidity(img, argv[1]);
+
     cv::imshow("img", img);
     cv::waitKey(0);
     return 0;
